b10ff785641b364b0e49c896efa28afd5c788304
wangxiao <wangxiao@patsnap.com>
2015-11-10 16:10:17 +0800

    catcher update

--- catcher.js
+++ catcher.js
@@ -1,48 +1,53 @@
-function loadJS(src, callback) {
-	var script = document.createElement('script'),
-		loaded;
-	script.setAttribute('src', src);
-	script.setAttribute('async', true);
-	if (callback) {
-		script.onreadystatechange = script.onload = function() {
-			if (!loaded) {
-				callback();
-			}
-			loaded = true;
-		};
+(function(global){
+
+	function loadJS(src, callback) {
+		var script = document.createElement('script'),
+			loaded;
+		script.setAttribute('src', src);
+		script.setAttribute('async', '');
+		if (callback) {
+			script.onreadystatechange = script.onload = function() {
+				if (!loaded) {
+					callback();
+				}
+				loaded = true;
+			};
+		}
+		document.getElementsByTagName('head')[0].appendChild(script);
 	}
-	document.getElementsByTagName('head')[0].appendChild(script);
-}
 
-loadJS("js/lib/raven.min.js", function() {
-	if(Raven) {
-		Raven.config('https://2ca5c9fc49a24072a962d85f51f3f924@app.getsentry.com/57824').install();
+	loadJS("js/lib/raven.min.js", function() {
+		if(Raven) {
+			Raven.config('https://2ca5c9fc49a24072a962d85f51f3f924@app.getsentry.com/57824').install();
 
-		Raven.setRelease('0.0.3');
+			Raven.setRelease('0.0.3');
 
-		function logException(ex, context) {
-			Raven.captureException(ex, {
-				extra: context
+			Raven.setUserContext({
+				email: 'matt@example.com',//user
+				id: '123'//request-id
 			});
-		}
-		function logInfo(str){
-			Raven.captureMessage(str);
-		}
 
-		if(window.console && console.error){
-			console.error = logException;
-		};
-		if(window.console && console.info){
-			console.info = logInfo;
-		};
+			function logException(ex, context) {
+				var opt = {};
+				context ? opt.extra = context : opt.extra = {'where':'internal'};
+				Raven.captureException(ex, opt);
+				console.log(ex);
+			}
 
-		window.onerror = function (message, file, line, column, errorObj) {
-			if (errorObj !== undefined) {
-				logException(errorObj, {'where': 'global'});
-			} else {
-				logException(message, {'where': 'global'});
+			if(window.console && console.error){
+				console.error = logException;
+			};
+
+			window.onerror = function (message, file, line, column, errorObj) {
+				if (errorObj !== undefined) {
+					logException(errorObj, {'where': 'global'});
+				} else {
+					logException(message, {'where': 'global'});
+				}
 			}
 		}
-	}
 
-});
+	});
+
+})(window);
+
